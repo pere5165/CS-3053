99aba51fea16118f161ad3de88ef7d65afd83d2d
a-selby <aaronbselby@gmail.com>
2017-04-13 19:30:26 -0500

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,9 +1,12 @@
 package application;
 
+import java.awt.Point;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.SequentialTransition;
 import javafx.animation.Timeline;
+import javafx.collections.ObservableList;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Arc;
 import javafx.scene.shape.ArcType;
 import javafx.util.Duration;
@@ -16,6 +19,8 @@ public class Robot {
 	private double xDisplacment;		 //These values	
 	private double yDisplacment;		 //are used to 
 	private double startAngleDisplacment;//build the animation
+	private boolean success;
+	
 	public Robot(double centerX, double centerY, double radius, double startAngle, double distance)
 	{
 		//IMPORTANT NOTE start angle should only be 0, 90, 180, or 270, determines direction 
@@ -30,66 +35,83 @@ public class Robot {
 		robot.setStartAngle(startAngle);
 		robot.setLength(180);
 		robot.setType(ArcType.ROUND);
-		this.distance = distance;		
+		robot.setFill(Color.SKYBLUE);
+		this.distance = distance;	
+		
 	}
 	public Arc getRobot() 
 	{
 		return robot;
 	};
-	private void forward() 
+	
+	
+	
+	private boolean forward() 
 	{	
-		KeyValue kv;
+		KeyValue kv = null;
 		KeyFrame kf = null;
+		int newX = (int)(robot.getCenterX() + xDisplacment);
+		int newY = (int)(robot.getCenterY() + yDisplacment);
 		
 		if((robot.getStartAngle() + startAngleDisplacment) < 0)	{	//if angle is negative right/left facing is reverse
 			if(((robot.getStartAngle() + startAngleDisplacment)%360) == 0) {	//robot is facing top of screen
-				kv = new KeyValue(robot.centerYProperty(), robot.getCenterY() - distance + yDisplacment);
-				kf = new KeyFrame(Duration.millis(1000), kv);
+				kv = new KeyValue(robot.centerYProperty(), robot.getCenterY() - distance + yDisplacment);			
+				newY = (int)(robot.getCenterY() - distance + yDisplacment);
 				yDisplacment += -distance;
 			}
 			else if((((robot.getStartAngle() + startAngleDisplacment)%360)%270) == 0) {	//robot is facing left side of screen								//robot is facing right side of screen
 				kv = new KeyValue(robot.centerXProperty(), robot.getCenterX() - distance + xDisplacment);
-				kf = new KeyFrame(Duration.millis(1000), kv);
-				xDisplacment += -distance;
-				
+				newX = (int)(robot.getCenterX() - distance + xDisplacment);
+				xDisplacment += -distance;				
 			}
 			else if((((robot.getStartAngle() + startAngleDisplacment)%360)%180) == 0) {	//robot is facing bottom of screen
 				kv = new KeyValue(robot.centerYProperty(), robot.getCenterY() + distance + yDisplacment);
-				kf = new KeyFrame(Duration.millis(1000), kv);
+				newY = (int)(robot.getCenterY() + distance + yDisplacment);
 				yDisplacment += distance;
 			}
 			else if((((robot.getStartAngle() + startAngleDisplacment)%360)%90) == 0){//robot is facing right side of screen
 				kv = new KeyValue(robot.centerXProperty(), robot.getCenterX() + distance + xDisplacment);
-				kf = new KeyFrame(Duration.millis(1000), kv);
+				newX = (int)(robot.getCenterX() + distance + xDisplacment);
 				xDisplacment += distance;
 			}
 		}
 		else {
 		if(((robot.getStartAngle() + startAngleDisplacment)%360) == 0) {	//robot is facing top of screen
 			kv = new KeyValue(robot.centerYProperty(), robot.getCenterY() - distance + yDisplacment);
-			kf = new KeyFrame(Duration.millis(1000), kv);
+			newY = (int)(robot.getCenterY() - distance + yDisplacment);
 			yDisplacment += -distance;
 		}
 		else if((((robot.getStartAngle() + startAngleDisplacment)%360)%270) == 0) {										//robot is facing right side of screen
 			kv = new KeyValue(robot.centerXProperty(), robot.getCenterX() + distance + xDisplacment);
-			kf = new KeyFrame(Duration.millis(1000), kv);
-			xDisplacment += distance;
-			
+			newX = (int)(robot.getCenterX() + distance + xDisplacment);
+			xDisplacment += distance;		
 		}
 		else if((((robot.getStartAngle() + startAngleDisplacment)%360)%180) == 0) {	//robot is facing bottom of screen
 			kv = new KeyValue(robot.centerYProperty(), robot.getCenterY() + distance + yDisplacment);
-			kf = new KeyFrame(Duration.millis(1000), kv);
+			newY = (int)(robot.getCenterY() + distance + yDisplacment);
 			yDisplacment += distance;
 		}
 		else if((((robot.getStartAngle() + startAngleDisplacment)%360)%90) == 0){//robot is facing left side of screen
 			kv = new KeyValue(robot.centerXProperty(), robot.getCenterX() - distance + xDisplacment);
-			kf = new KeyFrame(Duration.millis(1000), kv);
+			newX = (int)(robot.getCenterX() - distance + xDisplacment);
 			xDisplacment += -distance;
 		}
 		}
+		if(View.isValidPoint(new Point(newX, newY))) { //there is no wall in the way
+		kf = new KeyFrame(Duration.millis(1000), kv);
 		action.getKeyFrames().add(kf);
+		return true;	
+		}
+		else {	//there is a wall in the way
+			return false;
+		}
 		
 	}
+	
+	
+	
+	
+	
 	private void turnRight() 
 	{	//90 degree turn
 		KeyValue kv = new KeyValue(robot.startAngleProperty(), robot.getStartAngle() - 90 + startAngleDisplacment);
@@ -108,33 +130,56 @@ public class Robot {
 	/*the robot can execute a sequence of commands,
 	  given as a string array
 	  ex: bot.execute(new String[]{"forward","turn left", "forward", "turn right"})*/
-	public void execute(String[] commands) 
+	public boolean execute(ObservableList<String> commands) 
 	{	
 		xDisplacment = 0;
 		yDisplacment = 0;
 		startAngleDisplacment = 0;
+		success = true;
 		SequentialTransition path = new SequentialTransition();
 		for(String i : commands) 
 		{
 			action = new Timeline();
-			nextAction(i);
+			switch (i) {
+			case "Robot.MoveForward();":
+				success = forward();
+				break;
+			case "Robot.TurnLeft();":
+				turnLeft();
+				break;
+			case "Robot.TurnRight();":
+				turnRight();
+				break;
+			}
+			if(!success) break;	//if there is a wall in the way
 			path.getChildren().add(action);
 		}
+		path.setOnFinished(e -> { //.play() is an asynchronous call, this is needed to display success/failure after animation finishes
+			if(new Point((int)robot.getCenterX(),	//robot is on the goal at end of animation
+					(int)robot.getCenterY()).equals(new Point(525, 375))) {	
+				//IMPORTANT NOTE: need way to get goal location from view
+			View.showWinDialog();
+			}
+			else if(success && !(new Point((int)robot.getCenterX(),	//robot did not run into any walls but is NOT on the goal
+					(int)robot.getCenterY()).equals(new Point(525, 375)))) {
+				View.showIncompleteDialog();
+			}
+			else if(!success) {	//robot ran into a wall
+				View.showHitWallDialog();
+			}		
+		});
 		
-		path.play();		
+		path.play();
+		return success;
 	}
 	
-	private void nextAction(String command) {
-		switch (command) {
-		case "forward":
-			forward();
-			break;
-		case "turn left":
-			turnLeft();
-			break;
-		case "turn right":
-			turnRight();
-			break;
-		}
+	public void setLocation(double centerX, double centerY) 
+	{
+		robot.setCenterX(centerX);
+		robot.setCenterY(centerY);	
 	}
+	public Point getLocation() {
+		return new Point((int)robot.getCenterX(), (int)robot.getCenterY());
+	}
+	
 }
